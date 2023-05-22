07be743c7d3dba529feda299f3b49c10fc42da4d
unknown <laghmambilal49@gmail.com>
2023-05-22 01:14:41 +0100

    check input value

--- script.js
+++ script.js
@@ -1,80 +1,108 @@
 // var array = [];
 function add() {
-    var firsName = document.getElementById("firsName");
-    var lastName = document.getElementById("lastName");
+    var card = document.getElementById("card");
+    var name = document.getElementById("name");
     var age = document.getElementById("age");
     var notes = document.getElementById("notes");
-    if (firsName.value.trim() !== "" && lastName.value.trim() !== "" && age.value.trim() !== "" && notes.value.trim() !== "") {
-        var tbody = document.getElementById("tbody");
-        var createRow = document.createElement("tr");
-        var createTdOne = document.createElement("td");
-        var createTdTwo = document.createElement("td");
-        var createTdThree = document.createElement("td");
-        var createTdfoor = document.createElement("td");
-        var createTdfive = document.createElement("td");
-        createTdfive.setAttribute("class", "d-flex justify-content-center gap-1")
-        var createInputOne = document.createElement("input");
-        createInputOne.setAttribute("disabled", "disabled")
-        var createInputTwo = document.createElement("input");
-        createInputTwo.setAttribute("disabled", "disabled")
-        var createInputThree = document.createElement("input");
-        createInputThree.setAttribute("disabled", "disabled")
-        var createInputFoor = document.createElement("input");
-        createInputFoor.setAttribute("disabled", "disabled");
-        var createDelete = document.createElement("input");
-        var createUpdate = document.createElement("input");
-        createUpdate.setAttribute("class", "btn btn-primary w-50");
-        createUpdate.setAttribute("value", "Update");
-        createUpdate.setAttribute("type", "button");
-        createUpdate.setAttribute("id", "btnUpdate");
-        createDelete.setAttribute("class", "btn btn-danger w-50");
-        createDelete.setAttribute("value", "Delete");
-        createDelete.setAttribute("type", "button");
-        createDelete.setAttribute("id", "btnDelete");
-        createTdOne.appendChild(createInputOne);
-        createTdTwo.appendChild(createInputTwo);
-        createTdThree.appendChild(createInputThree);
-        createTdfoor.appendChild(createInputFoor);
-        createTdfive.appendChild(createUpdate);
-        createTdfive.appendChild(createDelete);
-        createRow.appendChild(createTdOne);
-        createRow.appendChild(createTdTwo);
-        createRow.appendChild(createTdThree);
-        createRow.appendChild(createTdfoor);
-        createRow.appendChild(createTdfive);
-        tbody.prepend(createRow);
-        var firsName = document.getElementById("firsName");
-        createInputOne.value = firsName.value
-        var lastName = document.getElementById("lastName");
-        createInputTwo.value = lastName.value
-        var age = document.getElementById("age");
-        createInputThree.value = age.value
-        var notes = document.getElementById("notes");
-        createInputFoor.value = notes.value
-        document.getElementById("firsName").value = "";
-        document.getElementById("lastName").value = "";
-        document.getElementById("age").value = "";
-        document.getElementById("notes").value = "";
-    } else {
-        alert("can't add empty value!")
+    if (card.value.trim() !== "" && name.value.trim() !== "" && age.value.trim() !== "" && notes.value.trim() !== "") {
+        if (!Number(name.value)) {
+            if (Number(age.value) > 0 && Number.isInteger(Number(age.value))) {
+                if (notes.value <= 20 && notes.value >= 0) {
+                    var tbody = document.getElementById("tbody");
+                    var createRow = document.createElement("tr");
+                    var createTdOne = document.createElement("td");
+                    var createTdTwo = document.createElement("td");
+                    var createTdThree = document.createElement("td");
+                    var createTdfoor = document.createElement("td");
+                    var createTdfive = document.createElement("td");
+                    createTdfive.setAttribute("class", "d-flex justify-content-center gap-1")
+                    var createInputOne = document.createElement("input");
+                    createInputOne.setAttribute("disabled", "disabled")
+                    var createInputTwo = document.createElement("input");
+                    createInputTwo.setAttribute("disabled", "disabled")
+                    var createInputThree = document.createElement("input");
+                    createInputThree.setAttribute("disabled", "disabled")
+                    var createInputFoor = document.createElement("input");
+                    createInputFoor.setAttribute("disabled", "disabled");
+                    var createDelete = document.createElement("input");
+                    var createUpdate = document.createElement("input");
+                    createUpdate.setAttribute("class", "btn btn-primary w-50");
+                    createUpdate.setAttribute("value", "Update");
+                    createUpdate.setAttribute("type", "button");
+                    createUpdate.setAttribute("id", "btnUpdate");
+                    createDelete.setAttribute("class", "btn btn-danger w-50");
+                    createDelete.setAttribute("value", "Delete");
+                    createDelete.setAttribute("type", "button");
+                    createDelete.setAttribute("id", "btnDelete");
+                    createTdOne.appendChild(createInputOne);
+                    createTdTwo.appendChild(createInputTwo);
+                    createTdThree.appendChild(createInputThree);
+                    createTdfoor.appendChild(createInputFoor);
+                    createTdfive.appendChild(createUpdate);
+                    createTdfive.appendChild(createDelete);
+                    createRow.appendChild(createTdOne);
+                    createRow.appendChild(createTdTwo);
+                    createRow.appendChild(createTdThree);
+                    createRow.appendChild(createTdfoor);
+                    createRow.appendChild(createTdfive);
+                    tbody.prepend(createRow);
+                    var card = document.getElementById("card");
+                    createInputOne.value = card.value
+                    var name = document.getElementById("name");
+                    createInputTwo.value = name.value
+                    var age = document.getElementById("age");
+                    createInputThree.value = age.value
+                    var notes = document.getElementById("notes");
+                    createInputFoor.value = notes.value
+                    document.getElementById("card").value = "";
+                    document.getElementById("name").value = "";
+                    document.getElementById("age").value = "";
+                    document.getElementById("notes").value = "";
+                } else {
+                    swal("Invalid Note!", "Please Enter Note between 0 and 20!", "error");
+                }
+            } else {
+                swal("Invalid Age!", "Please Enter Age greater than 0 and not float!", "error");
+            }
+        } else {
+            swal("Invalid Name!", "Please Enter just caracter!", "error");
+        }
+        swal("Successfully!", "You added all information by successfully!", "success");
+    }
+    else {
+        swal("Empty Value!", "Please Enter a Value!", "error");
     }
     createDelete.onclick = function () {
-        createRow.remove()
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this imaginary file!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    createRow.remove();
+                    swal("successfully deleted!", "you delete this student!", "success");
+                } else {
+                    swal("Cancel delete!", "The deletion has been cancelled!", "warning");
+                }
+            });
     }
     createUpdate.onclick = function () {
-        document.getElementById("firsName").value = createRow.children[0].firstChild.value;
-        document.getElementById("lastName").value = createRow.children[1].firstChild.value;
+        document.getElementById("card").value = createRow.children[0].firstChild.value;
+        document.getElementById("name").value = createRow.children[1].firstChild.value;
         document.getElementById("age").value = createRow.children[2].firstChild.value;
         document.getElementById("notes").value = createRow.children[3].firstChild.value;
         var updateConfirm = document.getElementById("updateConfirm")
         updateConfirm.innerHTML = "Update"
         updateConfirm.onclick = function () {
-            createRow.children[0].firstChild.value = document.getElementById("firsName").value;
-            createRow.children[1].firstChild.value = document.getElementById("lastName").value;
+            createRow.children[0].firstChild.value = document.getElementById("card").value;
+            createRow.children[1].firstChild.value = document.getElementById("name").value;
             createRow.children[2].firstChild.value = document.getElementById("age").value;
             createRow.children[3].firstChild.value = document.getElementById("notes").value;
-            document.getElementById("firsName").value = "";
-            document.getElementById("lastName").value = "";
+            document.getElementById("card").value = "";
+            document.getElementById("name").value = "";
             document.getElementById("age").value = "";
             document.getElementById("notes").value = "";
             updateConfirm.innerHTML = "Add";
@@ -87,12 +115,17 @@ var btn_search = document.getElementById("btn_search");
 btn_search.onclick = function () {
     var search = document.getElementById("search").value;
     var tbody = document.getElementById("tbody").children;
-    for (var i = 0; i < tbody.length; i++) {
-        if (tbody[i].firstChild.firstChild.value === search) {
-            tbody[i].style.display = "table-row";
-        } else {
-            tbody[i].style.display = "none";
+    if (search !== "") {
+        for (var i = 0; i < tbody.length; i++) {
+            if (tbody[i].firstChild.firstChild.value === search) {
+                tbody[i].style.display = "table-row";
+            }
+            else {
+                swal("Card Not Found!", "Please Enter Another Card!", "error");
+            }
         }
+    } else if (search == "") {
+        swal("Empty Value!", "Please Enter a Value!", "error");
     }
 }
 document.getElementById("search").onblur = function () {
@@ -134,6 +167,7 @@ function resetTable() {
     }
 }
 var tableSort = [];
+var sortingStatus = true;
 function sortNotes() {
     var tbody = document.getElementById("tbody").children;
     for (var i = 0; i < tbody.length; i++) {
@@ -141,13 +175,28 @@ function sortNotes() {
     }
     tableSort.sort(function (a, b) { return a - b });
     tableSort.reverse();
-    for (let i = 0; i < tbody.length - 1; i++) {
-        for (let j = 0; j < tbody.length - 1 - i; j++) {
-            if (Number(tbody[j].children[3].firstChild.value) < Number(tbody[j + 1].children[3].firstChild.value)) {
-                const temp = tbody[j];
-                tbody[j] = tbody[j + 1];
-                tbody[j + 1] = temp;
-                tbody[i].parentNode.insertBefore(tbody[j + 1], tbody[j]);
+    if (sortingStatus) {
+        sortingStatus = false;
+        for (let i = 0; i < tbody.length - 1; i++) {
+            for (let j = 0; j < tbody.length - 1 - i; j++) {
+                if (Number(tbody[j].children[3].firstChild.value) < Number(tbody[j + 1].children[3].firstChild.value)) {
+                    const temp = tbody[j];
+                    tbody[j] = tbody[j + 1];
+                    tbody[j + 1] = temp;
+                    tbody[i].parentNode.insertBefore(tbody[j + 1], tbody[j]);
+                }
+            }
+        }
+    } else {
+        sortingStatus = true;
+        for (let i = 0; i < tbody.length - 1; i++) {
+            for (let j = 0; j < tbody.length - 1 - i; j++) {
+                if (Number(tbody[j].children[3].firstChild.value) > Number(tbody[j + 1].children[3].firstChild.value)) {
+                    const temp = tbody[j];
+                    tbody[j] = tbody[j + 1];
+                    tbody[j + 1] = temp;
+                    tbody[i].parentNode.insertBefore(tbody[j + 1], tbody[j]);
+                }
             }
         }
     }
